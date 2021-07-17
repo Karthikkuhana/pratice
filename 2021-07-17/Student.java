52a6252d2ff6a1f2081333a8142b03ecda1d25c5
karthikkuchana <87562041+karthikkuchana@users.noreply.github.com>
2021-07-17 13:32:36 +0530

    Program about extends

---
+++ 2021-07-17/Student.java
@@ -0,0 +1,23 @@
+class Parents
+ {
+   public  void marriage()
+   { 
+    System.out.println("karthik");
+   }
+ }
+class son extends Parents
+{
+  public void marriage()
+  {
+    System. out.println("shruthi");
+  }
+  
+}
+public class ppp
+{
+  public static void main(String args[])
+  {
+  son s1=new son();
+  s1.marriage ();
+  }
+}
