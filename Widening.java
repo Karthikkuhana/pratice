bef1e8776c16cabe5fe103b048d3e6ed02234f95
karthikkuchana <87562041+karthikkuchana@users.noreply.github.com>
2021-07-27 18:16:20 +0530

    Create Widening.java

---
+++ Widening.java
@@ -0,0 +1,14 @@
+
+lass wedining
+ {
+   public static void main(String args[])
+   {
+     int i=2234;
+     long l=i;
+     float f=l;
+     double d=f;
+     System.out.println(l);
+     System.out.println(f);
+     System.out.println(d);
+   }
+ }
