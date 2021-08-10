3b3d4b506c69f1153b683c2287213c2e389f4617
karthikkuchana <87562041+karthikkuchana@users.noreply.github.com>
2021-08-10 23:39:59 +0530

    Create Student info.java

---
+++ Student info.java
@@ -0,0 +1,21 @@
+
+public class Studentinfo
+ {
+   public static void main(String args[])
+   { 
+    String sname="shruthi";
+    String scollage="vaggdevi";
+    String saddress="11-28-218/2,kothawada,warangal";
+    byte sage=25;
+    float sper=59.9f;
+    short syop=2018;
+    long scontact=9398679552l;
+    System.out.println(sname);
+    System.out.println(scollage);
+    System.out.println(saddress);
+    System.out.println(sage);
+    System.out.println(sper);
+    System.out.println(syop);
+    System.out.println(scontact);
+   }
+ }
